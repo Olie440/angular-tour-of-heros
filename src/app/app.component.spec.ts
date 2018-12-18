fd8d7c9277b65577377b7dfed1b1ccd1c2657b23
Oliver Rushworth <oliver@oliverushworth.uk>
2018-12-18 16:44:24 +0000

    [Tutorial] - Update title and tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-tour-of-heros'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-tour-of-heros');
+    expect(app.title).toEqual('Tour of Heros');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-tour-of-heros!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Tour of Heros!');
   });
 });
