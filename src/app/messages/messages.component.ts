69872be600adbbc96c271ade48a92d7bda918c7e
Olie440 <accounts+github@oliverrushworth.uk>
2022-06-12 10:32:41 +0000

    test-changes

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { MessageService } from '../message.service';
+import { Component, OnInit } from "@angular/core";
+import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  selector: "app-messages",
+  templateUrl: "./messages.component.html",
+  styleUrls: ["./messages.component.css"],
 })
 export class MessagesComponent implements OnInit {
-  constructor(public messageService: MessageService) {}
+  constructor(
+    public messageService: MessageService,
+    private heroService: HeroService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.heroService.getHeroes().subscribe((heros) => console.log(heros));
+  }
 }
